1085884706f7b6f3367831d8ed3fe0df72b9b0aa
Ling Chao <iwelshaha@gmail.com>
2020-11-21 12:28:49 +0100

    new version

--- password.py
+++ password.py
@@ -1,10 +1,15 @@
 ps = 'a123456'
 x =3
 while x > 0:
+	x = x-1
 	ps1 = input('請輸入密碼(你有三次機會):')
 	if ps1 != ps:
-		print('密碼錯誤','還有', x-1, '次機會')
-	x = x-1
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有',x,'次機會!')
+		else:
+			print('沒有機會了！')
+	
 	if ps1 == ps:
 		print('登入成功')
 		break
